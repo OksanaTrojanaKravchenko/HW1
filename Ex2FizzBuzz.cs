9312bee402ea96f08a16945ee38d2e00c7322f9c
OksanaTrojanaKravchenko <53123299+OksanaTrojanaKravchenko@users.noreply.github.com>
2019-07-24 09:55:29 +0300

    Add files via upload

--- Ex2FizzBuzz.cs
+++ Ex2FizzBuzz.cs
@@ -8,44 +8,70 @@ namespace HW1
     {
         public static void FizzBuzz()
         {
-            int[] array = new int[100];
+            Console.WriteLine(OutputArray(CreateArray(100)));
+            Console.WriteLine("\nPlease enter upper limit for your array\n");
+            string maxValueString = Console.ReadLine();
+            int maxValueInt = int.Parse(maxValueString);
+            Console.WriteLine(OutputArray(CreateArray(maxValueInt)));
+        }
+
+        static int[] CreateArray(int elementsQuantity)
+        {
+            int[] array = new int[elementsQuantity];
             for (int i = 0; i < array.Length; i++)
             {
                 array[i] = i + 1;
             }
+            return array;
+        }
+
+        static string OutputArray(int[]array)
+        {
+            string result = "";
             for (int i = 0; i < array.Length; i++)
             {
-                DivisibleByMethod(array[i]);
+                result += DivisibleByMethod(array[i]) + "\n";
             }
-            Console.ReadLine();
-
-            Console.WriteLine("\nPlease enter upper limit for your array\n");
-
-            string maxValueString = Console.ReadLine();
-            int maxValueInt = int.Parse(maxValueString);
-            int[] array2 = new int[maxValueInt];
-            for (int y = 0; y < array2.Length; y++)
-            {
-                array2[y] = y + 1;
-            }
-            for (int y = 0; y < maxValueInt; y++)
-            {
-                DivisibleByMethod(array2[y]);
-            }
+            return result;
         }
-        static void DivisibleByMethod(int numberToDoFizzBuzz)
+
+        static string DivisibleByMethod(int numberToDoFizzBuzz)
         {
+            //variant 1
             int zero = 0;
-            if (zero == numberToDoFizzBuzz % 15)
-                    Console.WriteLine("FizzBuzz");
-            else if (zero == numberToDoFizzBuzz % 3 && zero != numberToDoFizzBuzz % 5)
-                    Console.WriteLine("Fizz");
+            string a = null;
+            if (numberToDoFizzBuzz % 3 == zero)
+            {
+                a += "Fizz";
+            }
+            if (numberToDoFizzBuzz % 5 == zero)
+            {
+                a += "Buzz";
+            }
+            return a ?? numberToDoFizzBuzz.ToString();
 
-            else if (zero != numberToDoFizzBuzz % 3 && zero == numberToDoFizzBuzz % 5)
-                    Console.WriteLine("Buzz");
+            //variant 2
+            var b = "";
+            //if (numberToDoFizzBuzz % 3 == zero)
+            //{
+            //    b += "Fizz";
+            //}
+            //if (numberToDoFizzBuzz % 5 == zero)
+            //{
+            //    b += "Buzz";
+            //}
+            //return (b != "") ? b : numberToDoFizzBuzz.ToString();
 
-            else
-                    Console.Write(numberToDoFizzBuzz + " ");
+            //variant 3
+            //int zero = 0;
+            //if (numberToDoFizzBuzz % 15 == zero)
+            //    return "FizzBuzz";
+            //else if (numberToDoFizzBuzz % 3 == zero)
+            //    return "Fizz";
+            //else if (numberToDoFizzBuzz % 5 == zero)
+            //    return  "Buzz";
+            //else
+            //    return numberToDoFizzBuzz.ToString();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,28 +1,41 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 
 namespace HW1
 {
     class Program
     {
+        //Visitor Visitor { get; } = "";
         static void Main(string[] args)
         {
             Console.WriteLine("How you prefer to be called?");
             Visitor visitor = new Visitor();
             visitor.ReadNameFromConsole();
-            Console.WriteLine("Hello " + visitor.Name + "!");
+            Console.WriteLine("Hello " + visitor.Name + "!"+"\n\nEnd of the Greeting program" + "\n\nPress any key to start the next one");
             Console.ReadLine();
-
+            //visitor.Name = "";
             Ex2FizzBuzz.FizzBuzz();
-            Console.ReadLine();
+            Console.WriteLine("\nEnd of the FizzBuzz program" + "\n\nPress any key to start the next one");
 
+            Console.ReadLine();
+            //{
+            //    var a = 5;
+            //}
             Console.WriteLine("Guess the number");
             GuessGame.Guess();
         }
     }
+
     class Visitor
     {
         public string Name;
-        public void ReadNameFromConsole() =>
-        Name = Console.ReadLine();
+
+        public void ReadNameFromConsole() 
+            => Name = Console.ReadLine();
     }
 }
+
